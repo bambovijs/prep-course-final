b71a3269fe23b41a1b3329a0818f60d1b5170b7c
Raivo Bambis <raivo@bambis.lv>
2024-02-02 18:36:15 +0200

    tictactoe

--- 00-tic-tac-toe/src/Game.ts
+++ 00-tic-tac-toe/src/Game.ts
@@ -14,8 +14,34 @@ export class Game {
 
   getWinner(): XO {
     const cells = this.getCells();
+    //rows
     if (cells[0] === cells[1] && cells[1] === cells[2]) {
       return cells[0];
+    }
+    if (cells[3] === cells[4] && cells[4] === cells[5]) {
+      return cells[3];
+    }
+    if (cells[6] === cells[7] && cells[7] === cells[8]) {
+      return cells[6];
+    }
+
+    //cols
+    if (cells[0] === cells[1] && cells[3] === cells[6]) {
+      return cells[0];
+    }
+    if (cells[1] === cells[4] && cells[4] === cells[7]) {
+      return cells[1];
+    }
+    if (cells[2] === cells[5] && cells[5] === cells[8]) {
+      return cells[2];
+    }
+
+    //diognales
+    if (cells[0] === cells[4] && cells[4] === cells[8]) {
+      return cells[0];
+    }
+    if (cells[2] === cells[4] && cells[4] === cells[6]) {
+      return cells[2];
     }
 
     return "-";
